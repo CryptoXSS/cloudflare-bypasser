e538795bbb4e72a24b845522820906257b0a0308
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2021-10-06 21:56:01 -0300

    Update test2.js

--- tests/test2.js
+++ tests/test2.js
@@ -5,7 +5,7 @@
 const CloudflareBypasser = require('../');
 const helpers = require('../lib/helpers');
 
-const URL = 'https://market.csgo.com/';
+const URL = 'https://gota.io/';
 
 let cloudflare = new CloudflareBypasser({
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
